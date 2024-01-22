aadf166afc01d8ff95d2ed6aa6115d371fbc38c7
wizardy <wizardhassan99@gmail.com>
2024-01-22 20:25:29 +0100

    commit

--- sgi-frontend/src/app/pages/auth/login/login.component.ts
+++ sgi-frontend/src/app/pages/auth/login/login.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  login() {
+    throw new Error('Method not implemented.');
+  }
 
 }
