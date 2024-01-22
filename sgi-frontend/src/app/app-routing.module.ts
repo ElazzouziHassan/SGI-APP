09616cee8a27816ace3ea7bd54e4ee689732a17e
wizardy <wizardhassan99@gmail.com>
2024-01-22 19:51:58 +0100

    commit

--- sgi-frontend/src/app/app-routing.module.ts
+++ sgi-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
 
 
 const routes: Routes = [
-  {path:'login', component: LoginComponent},
-  {path:'register', component: RegisterComponent},
+  {path:'login', title:'Login to EST SB | UCD EL JADIDA', component: LoginComponent},
+  {path:'register',title:'Register to EST SB | UCD EL JADIDA', component: RegisterComponent},
+  {path:'inscription',title:'Inscrit to EST SB | UCD EL JADIDA', component: InscriptionComponent},
   {path: '', redirectTo: 'register', pathMatch: 'full'},
   {path: '**', title:'Error ! | Page Not Found', component: NotFoundPageComponent},
 ];
