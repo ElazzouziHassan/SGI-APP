97ea8bcdd1d91b18496b67f5544ae549308136e9
wizardy <wizardhassan99@gmail.com>
2024-01-22 20:25:39 +0100

    commit

--- sgi-frontend/src/app/pages/auth/register/register.component.ts
+++ sgi-frontend/src/app/pages/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  register() {
+    throw new Error('Method not implemented.');
+  }
 
 }
