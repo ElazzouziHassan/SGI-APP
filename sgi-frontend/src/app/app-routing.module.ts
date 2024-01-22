ccd3e18b6e697629d057b9cb1cfb24084966d7e9
wizardy <wizardhassan99@gmail.com>
2024-01-22 19:45:46 +0100

    commit

--- sgi-frontend/src/app/app-routing.module.ts
+++ sgi-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { registerLocaleData } from '@angular/common';
+import { RegisterComponent } from './pages/auth/register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'login', component: LoginComponent},
+  {path:'**', component: RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
