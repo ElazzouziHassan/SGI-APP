00eba72842e94c747aae0393802976e814fb3945
wizardy <wizardhassan99@gmail.com>
2024-01-22 19:48:49 +0100

    commit

--- sgi-frontend/src/app/app-routing.module.ts
+++ sgi-frontend/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/login/login.component';
-import { registerLocaleData } from '@angular/common';
 import { RegisterComponent } from './pages/auth/register/register.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 
 const routes: Routes = [
   {path:'login', component: LoginComponent},
-  {path:'**', component: RegisterComponent},
+  {path:'register', component: RegisterComponent},
+  {path: '', redirectTo: 'register', pathMatch: 'full'},
+  {path: '**', title:'Error ! | Page Not Found', component: NotFoundPageComponent},
 ];
 
 @NgModule({
