a72452c18421b05636f48581b2048728bbefc114
wizardy <wizardhassan99@gmail.com>
2024-01-22 19:32:32 +0100

    auth login page

--- sgi-frontend/src/app/app.module.ts
+++ sgi-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
