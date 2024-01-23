8ed25e2fb3bdf5221aa2da0349cfbd832e2a2711
wizardy <wizardhassan99@gmail.com>
2024-01-23 17:15:53 +0100

    commit

--- sgi-frontend/src/app/app-routing.module.ts
+++ sgi-frontend/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { RegisterComponent } from './pages/auth/register/register.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { InscriptionComponent } from './pages/inscription/inscription.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { ResetPasswordComponent } from './pages/auth/reset-password/reset-password.component';
 
 
 const routes: Routes = [
   {path:'login', title:'Login to EST SB | UCD EL JADIDA', component: LoginComponent},
+  {path:'reset-password', title:'Reset Your Password | EST SB', component: ResetPasswordComponent},
   {path:'registration',title:'Register to EST SB | UCD EL JADIDA', component: RegisterComponent},
   {path:'inscription',title:'Inscrit to EST SB | UCD EL JADIDA', component: InscriptionComponent},
   {path:'profile',title:'EST SB | UCD EL JADIDA', component: ProfileComponent},
