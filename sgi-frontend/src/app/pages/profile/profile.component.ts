73fa4f2f40759455e75b2ad8557415902b2e6e38
wizardy <wizardhassan99@gmail.com>
2024-01-23 16:05:29 +0100

    commit

--- sgi-frontend/src/app/pages/profile/profile.component.ts
+++ sgi-frontend/src/app/pages/profile/profile.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-  
+  user = {
+    nom : 'El azzouzi',
+    prenom: 'Hassan',
+    specialite: 'Web Developer',
+    telephone: '+212 648540458',
+    email: 'ezhassan.info@gmail.com',
+    adresse: 'Hay elmatar El Jadida',
+    
+  }
 }
