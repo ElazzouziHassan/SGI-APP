74e53c87f82f9810bada8c881ce6c689b3677672
wizardy <wizardhassan99@gmail.com>
2024-01-23 17:39:12 +0100

    commit

--- sgi-frontend/src/app/app-routing.module.ts
+++ sgi-frontend/src/app/app-routing.module.ts
@@ -7,15 +7,23 @@ import { InscriptionComponent } from './pages/inscription/inscription.component'
 import { ProfileComponent } from './pages/profile/profile.component';
 import { ResetPasswordComponent } from './pages/auth/reset-password/reset-password.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
 const routes: Routes = [
   {path:'login', title:'Login to EST SB | UCD EL JADIDA', component: LoginComponent},
   {path:'reset-password', title:'Reset Your Password | EST SB', component: ResetPasswordComponent},
   {path:'registration',title:'Register to EST SB | UCD EL JADIDA', component: RegisterComponent},
-  {path:'inscription',title:'Inscrit to EST SB | UCD EL JADIDA', component: InscriptionComponent},
-  {path:'home',title:'Welcome to EST SB | UCD EL JADIDA', component: HomeComponent},
-  {path:'profile',title:'EST SB | UCD EL JADIDA', component: ProfileComponent},
+  {
+    path:'home',title:'Welcome to EST SB | UCD EL JADIDA', component: HomeComponent,
+    children:[
+      {path:'inscription',title:'Inscrit to EST SB | UCD EL JADIDA', component: InscriptionComponent},
+      {path:'profile',title:'Welcome To Your Profile  | EST SB', component: ProfileComponent},
+      {path:'accueil',title:'Welcome ! | EST SB', component: AccueilComponent},
+      {path:'contact',title:'Contactez-nous | EST SB', component: ContactComponent},
+    ]
+  },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {path: '**', title:'Error ! | Page Not Found', component: NotFoundPageComponent},
 ];
