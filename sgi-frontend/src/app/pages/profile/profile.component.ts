2cc6f9a27855c3314123fca382c7adce9516ee90
wizardy <wizardhassan99@gmail.com>
2024-01-23 16:32:08 +0100

    commit

--- sgi-frontend/src/app/pages/profile/profile.component.ts
+++ sgi-frontend/src/app/pages/profile/profile.component.ts
@@ -13,6 +13,26 @@ export class ProfileComponent {
     telephone: '+212 648540458',
     email: 'ezhassan.info@gmail.com',
     adresse: 'Hay elmatar El Jadida',
-    
+
   }
+  baccalaureat = {
+    etablissement: "IBN KHALDOUNE",
+    directiction: "Casablanca-Settat",
+    session: "juin 2016",
+    serie: "Scientifiques",
+    filier: "Sc. Math option A",
+    mention: "Passable",
+    moyene:"11.16"
+  }
+  diplome = {
+    etablissement: "ISTA CITE DE L'AIR",
+    directiction: "Casablanca-Settat",
+    session: "2023",
+    type: "Technicien speacialise",
+    filier: "Developpement web",
+    mention: "Excelent",
+    moyene:"17, 82",
+    nb_annees: "2 ans"
+  }
+
 }
