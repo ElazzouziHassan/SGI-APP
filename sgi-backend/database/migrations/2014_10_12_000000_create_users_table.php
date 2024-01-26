9a17d0d69423eacd96ce2e656f43a75e23a3bc33
wizardy <wizardhassan99@gmail.com>
2024-01-26 23:04:33 +0100

    commit

---
+++ sgi-backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom_fr');
+            $table->string('nom_ar');
+            $table->string('prenom_fr');
+            $table->string('prenom_ar');
+            $table->date('dateNaissance');
+            $table->string('email')->unique();
+            $table->string('telephone');
+            $table->boolean('role');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
