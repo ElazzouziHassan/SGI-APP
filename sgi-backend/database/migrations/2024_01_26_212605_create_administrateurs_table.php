ab5f8ad128f85edc036de639352274c9932113fe
wizardy <wizardhassan99@gmail.com>
2024-01-26 22:32:03 +0100

    commit

--- sgi-backend/database/migrations/2024_01_26_212605_create_administrateurs_table.php
+++ sgi-backend/database/migrations/2024_01_26_212605_create_administrateurs_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('administrateurs', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('utilisateur_id')->constrained();
+            $table->string('username');
+            $table->string('password');
             $table->timestamps();
         });
     }
