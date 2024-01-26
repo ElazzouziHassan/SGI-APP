5edb9beba0e7df7ea9d569323d941b44590a4c52
wizardy <wizardhassan99@gmail.com>
2024-01-26 22:32:12 +0100

    commit

--- sgi-backend/database/migrations/2024_01_26_212638_create_etudiants_table.php
+++ sgi-backend/database/migrations/2024_01_26_212638_create_etudiants_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('etudiants', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('utilisateur_id')->constrained();
+            $table->string('username');
+            $table->string('password');
+            $table->string('CodeMassar');
+            $table->string('CIN');
+            $table->text('adresse_fr');
+            $table->text('adresse_ar');
             $table->timestamps();
         });
     }
