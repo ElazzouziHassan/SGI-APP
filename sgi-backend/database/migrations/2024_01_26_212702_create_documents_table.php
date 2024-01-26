ac9e3e631f2d402e7152f902addcd2641b7aba0a
wizardy <wizardhassan99@gmail.com>
2024-01-26 22:32:21 +0100

    commit

--- sgi-backend/database/migrations/2024_01_26_212702_create_documents_table.php
+++ sgi-backend/database/migrations/2024_01_26_212702_create_documents_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('etudiant_id')->constrained();
+            $table->string('documentBacV');
+            $table->string('documentBacR');
+            $table->string('documentDiplome');
+            $table->string('documentCINV');
+            $table->string('documentCINR');
+            $table->string('documentRelvetNoteBac');
+            $table->string('documentRelNoteS1');
+            $table->string('documentRelNoteS2');
+            $table->string('documentRelNoteS3');
+            $table->string('documentRelNoteS4');
+            $table->string('photo');
+            $table->string('demandeManuscript');
             $table->timestamps();
         });
     }
