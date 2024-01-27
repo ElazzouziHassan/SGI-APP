49da967e6164dfdcc243b927e4b8c17c958c8bb5
wizardy <wizardhassan99@gmail.com>
2024-01-27 12:11:04 +0100

    commit

--- sgi-backend/database/migrations/2024_01_26_212630_create_services_etudiants_table.php
+++ sgi-backend/database/migrations/2024_01_26_212630_create_services_etudiants_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->string('email');
-            $table->string('password');
             $table->timestamps();
         });
     }
