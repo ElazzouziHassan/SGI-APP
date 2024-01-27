b0017b86a60a37959d7247d52d370d0760ec7791
wizardy <wizardhassan99@gmail.com>
2024-01-27 17:08:34 +0100

    commit

--- sgi-backend/app/Http/Controllers/UserController.php
+++ sgi-backend/app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -11,7 +13,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return response()->json(['users' => $users], 200);
     }
 
     /**
@@ -27,7 +30,29 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nom_fr' => 'required|string',
+            'prenom_fr' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'telephone' => 'required',
+        ]);
+        $user = User::create([
+            'nom_fr' => $request->input('nom-fr'),
+            'nom_ar' => $request->input('nom-ar'),
+            'prenom_fr' => $request->input('prenom-fr'),
+            'prenom_ar' => $request->input('prenom-ar'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+            'telephone' => $request->input('telephone'),
+            'dateNaissance' => $request->input('date-naissance'), 
+            'CodeMassar' => $request->input('code-massar'),
+            'CIN' => $request->input('cin'),
+            'adresse_fr' => $request->input('adresse-fr'),
+            'adresse_ar' => $request->input('adresse-ar'),
+        ]);
+
+        return response()->json(['user' => $user], 201);
     }
 
     /**
@@ -35,7 +60,13 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        return response()->json(['user' => $user], 200);
     }
 
     /**
@@ -51,7 +82,33 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        $request->validate([
+            'nom_fr' => 'required|string',
+            'prenom_fr' => 'required|string',
+            'email' => 'required|email|unique:users,email,' . $id,
+            'telephone' => 'required',
+        ]);
+
+        $user->update([
+            'nom_fr' => $request->input('nom-fr'),
+            'nom_ar' => $request->input('nom-ar'),
+            'prenom_fr' => $request->input('prenom-fr'),
+            'prenom_ar' => $request->input('prenom-ar'),
+            'telephone' => $request->input('telephone'),
+            'dateNaissance' => $request->input('date-naissance'), 
+            'CodeMassar' => $request->input('code-massar'),
+            'CIN' => $request->input('cin'),
+            'adresse_fr' => $request->input('adresse-fr'),
+            'adresse_ar' => $request->input('adresse-ar'),
+        ]);
+
+        return response()->json(['user' => $user], 200);
     }
 
     /**
@@ -59,6 +116,15 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted successfully'], 200);
     }
+    
 }
