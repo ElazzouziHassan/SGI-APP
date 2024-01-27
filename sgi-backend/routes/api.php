eb7b31a35d3ccc3034eed9923e914bd145fa0a25
wizardy <wizardhassan99@gmail.com>
2024-01-27 16:11:45 +0100

    commit

--- sgi-backend/routes/api.php
+++ sgi-backend/routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('users', 'UserController');
+Route::resource('feedback', 'FeedbackController');
+Route::resource('documents', 'DocumentController');
+Route::resource('formations', 'FormationController');
+Route::resource('inscriptions', 'InscriptionController');
+Route::resource('baccalaureats', 'BaccalaureatController');
+Route::resource('diplomes', 'DiplomeController');
+
