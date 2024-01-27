710984df568c4d7f66f94e9a9fc7e1736d1be01a
wizardy <wizardhassan99@gmail.com>
2024-01-27 18:16:20 +0100

    commit

--- sgi-frontend/src/app/pages/auth/login/login.component.ts
+++ sgi-frontend/src/app/pages/auth/login/login.component.ts
@@ -3,11 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  public form = {
+    email: null,
+    password: null
+  };
+  constructor() {}
   login() {
-    throw new Error('Method not implemented.');
+    console.log(this.form);
+    // TODO: Implement login functionality.
   }
-
 }
