96bf7138e1cd987398b5aa431ad9d2751f4473e7
wizardy <wizardhassan99@gmail.com>
2024-01-27 16:01:26 +0100

    Diplomes Model : Updates on (Fillable, relationships)

--- sgi-backend/app/Models/Diplomes.php
+++ sgi-backend/app/Models/Diplomes.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Diplomes extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id', 
+        'Etablissement', 
+        'Direction_provinciale', 
+        'Promotion', 
+        'type', 
+        'Specialite', 
+        'nb_annees',
+        'Moyene_s1', 
+        'Moyene_s2', 
+        'Moyene_s3', 
+        'Moyene_s4', 
+        'Moyene_generale', 
+        'nb_mention'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
