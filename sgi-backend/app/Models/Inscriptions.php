40f2d94cfab55fc1bbb008fe27936d85fc476ac5
wizardy <wizardhassan99@gmail.com>
2024-01-27 12:23:20 +0100

    commit

---
+++ sgi-backend/app/Models/Inscriptions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Inscriptions extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'etudiant_id', 'formation_id', 'status',
+    ];
+
+    public function etudiant()
+    {
+        return $this->belongsTo(Etudiant::class);
+    }
+
+    public function formation()
+    {
+        return $this->belongsTo(Formation::class);
+    }
+}
