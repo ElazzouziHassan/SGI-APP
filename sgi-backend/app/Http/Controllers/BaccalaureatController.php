0aa5e6294af7c75d498fba56c2c0cbe117253b55
wizardy <wizardhassan99@gmail.com>
2024-01-27 17:08:19 +0100

    commit

--- sgi-backend/app/Http/Controllers/BaccalaureatController.php
+++ sgi-backend/app/Http/Controllers/BaccalaureatController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Baccalaureats;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class BaccalaureatController extends Controller
@@ -11,7 +13,8 @@ class BaccalaureatController extends Controller
      */
     public function index()
     {
-        //
+        $baccalaureats = Baccalaureats::all();
+        return response()->json(['baccalaureats' => $baccalaureats], 200);
     }
 
     /**
@@ -27,7 +30,36 @@ class BaccalaureatController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'Etablissement' => 'required|string',
+            'Direction_provinciale' => 'required|string',
+            'Session' => '',
+            'Serie' => '',
+            'Filiere' => 'string',
+            'Mention' => '',
+            'Moyene_generale' => '',
+        ]);
+
+        $user = User::find($request->input('user_id'));
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        $baccalaureat = new Baccalaureats();
+        $baccalaureat->user_id = $user->id;
+        $baccalaureat->Etablissement = $request->input('Etablissement');
+        $baccalaureat->Direction_provinciale = $request->input('Direction_provinciale');
+        $baccalaureat->Promotion = $request->input('Session');
+        $baccalaureat->type = $request->input('Serie');
+        $baccalaureat->Specialite = $request->input('Filiere');
+        $baccalaureat->Specialite = $request->input('Mention');
+        $baccalaureat->Moyene_generale = $request->input('Moyene_generale');
+
+        $baccalaureat->save();
+
+        return response()->json(['baccalaureat' => $baccalaureat], 201);
     }
 
     /**
@@ -35,7 +67,13 @@ class BaccalaureatController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $baccalaureat = Baccalaureats::find($id);
+
+        if (!$baccalaureat) {
+            return response()->json(['error' => 'Baccalaureat not found'], 404);
+        }
+
+        return response()->json(['baccalaureat' => $baccalaureat], 200);
     }
 
     /**
@@ -51,7 +89,41 @@ class BaccalaureatController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $baccalaureat = Baccalaureats::find($id);
+
+        if (!$baccalaureat) {
+            return response()->json(['error' => 'Baccalaureat not found'], 404);
+        }
+
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'Etablissement' => 'required|string',
+            'Direction_provinciale' => 'required|string',
+            'Session' => '',
+            'Serie' => '',
+            'Filiere' => 'string',
+            'Mention' => '',
+            'Moyene_generale' => '',
+        ]);
+
+        $user = User::find($request->input('user_id'));
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        $baccalaureat->user_id = $user->id;
+        $baccalaureat->Etablissement = $request->input('Etablissement');
+        $baccalaureat->Direction_provinciale = $request->input('Direction_provinciale');
+        $baccalaureat->Promotion = $request->input('Session');
+        $baccalaureat->type = $request->input('Serie');
+        $baccalaureat->Specialite = $request->input('Filiere');
+        $baccalaureat->Specialite = $request->input('Mention');
+        $baccalaureat->Moyene_generale = $request->input('Moyene_generale');
+
+        $baccalaureat->save();
+
+        return response()->json(['baccalaureat' => $baccalaureat], 200);
     }
 
     /**
@@ -59,6 +131,14 @@ class BaccalaureatController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $baccalaureat = Baccalaureats::find($id);
+
+        if (!$baccalaureat) {
+            return response()->json(['error' => 'Baccalaureat not found'], 404);
+        }
+
+        $baccalaureat->delete();
+
+        return response()->json(['message' => 'Baccalaureat deleted successfully'], 200);
     }
 }
