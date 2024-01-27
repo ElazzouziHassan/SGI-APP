6c237844e2297f26d93d4de680fd9b7f0ccb6902
wizardy <wizardhassan99@gmail.com>
2024-01-27 16:01:46 +0100

    Feedback Model : Updates on (Fillable, relationships)

--- sgi-backend/app/Models/Feedback.php
+++ sgi-backend/app/Models/Feedback.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Feedback extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id', 'content',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
