f09b3c63c75869544e836f495261ab342a4458a5
wizardy <wizardhassan99@gmail.com>
2024-01-27 12:10:52 +0100

    commit

--- sgi-backend/app/Models/User.php
+++ sgi-backend/app/Models/User.php
@@ -19,9 +19,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        
     ];
 
     /**
