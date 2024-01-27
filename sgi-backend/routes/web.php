42b1137e9f69190ae9327b5127adcda00c71aa0c
wizardy <wizardhassan99@gmail.com>
2024-01-27 12:29:57 +0100

    commit

--- sgi-backend/routes/web.php
+++ sgi-backend/routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+use App\Http\Controllers\AdministrateurController;
+use App\Http\Controllers\BaccalaureatsController;
+use App\Http\Controllers\DiplomesController;
+use App\Http\Controllers\DocumentsController;
+use App\Http\Controllers\EtudiantController;
+use App\Http\Controllers\FeedbackController;
+use App\Http\Controllers\FormationController;
+use App\Http\Controllers\InscriptionsController;
+use App\Http\Controllers\ServicesEtudiantController;
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +27,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('utilisateurs', UserController::class);
+Route::resource('administrateurs', AdministrateurController::class);
+Route::resource('etudiants', EtudiantController::class);
+Route::resource('services_etudiants', ServicesEtudiantController::class);
+Route::resource('feedbacks', FeedbackController::class);
+Route::resource('documents', DocumentsController::class);
+Route::resource('formations', FormationController::class);
+Route::resource('diplomes', DiplomesController::class);
+Route::resource('baccalaureats', BaccalaureatsController::class);
+Route::resource('inscriptions', InscriptionsController::class);
