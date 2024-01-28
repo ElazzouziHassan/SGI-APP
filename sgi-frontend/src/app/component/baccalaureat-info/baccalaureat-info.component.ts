bad33180aae28afa82e8eb9901d3d7726d0b9104
wizardy <wizardhassan99@gmail.com>
2024-01-28 18:29:13 +0100

    commit

--- sgi-frontend/src/app/component/baccalaureat-info/baccalaureat-info.component.ts
+++ sgi-frontend/src/app/component/baccalaureat-info/baccalaureat-info.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrl: './baccalaureat-info.component.scss'
 })
 export class BaccalaureatInfoComponent {
-
+  public formData = {
+    etablissement: null,
+    directionProvinciale: null,
+    session: null,
+    serie: null,
+    filiere: null,
+    mention: null,
+    moyenneGenerale: null,
+    
+  };
+  constructor() {}
+  submitForm() {
+    console.log(this.formData);
+    // TODO: Implement form submission functionality.
+  }
+  get isEtablissementEmpty() {
+    return !this.formData.etablissement;
+  }
+  get isDirectionProvincialeEmpty() {
+    return !this.formData.directionProvinciale;
+  }
+  get isSessionEmpty() {
+    return !this.formData.session;
+  }
+  get isSerieEmpty() {
+    return !this.formData.serie;
+  }
+  get isFiliereEmpty() {
+    return !this.formData.filiere;
+  }
+  get isMentionEmpty() {
+    return !this.formData.mention;
+  }
+  get isMoyenneGeneraleEmpty() {
+    return !this.formData.moyenneGenerale;
+  }
 }
