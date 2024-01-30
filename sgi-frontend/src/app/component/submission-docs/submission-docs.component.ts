3ceae1369c0a1ae0735aa100d1acb1f84123ab22
wizardy <wizardhassan99@gmail.com>
2024-01-30 12:09:13 +0100

    commit

--- sgi-frontend/src/app/component/submission-docs/submission-docs.component.ts
+++ sgi-frontend/src/app/component/submission-docs/submission-docs.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './submission-docs.component.scss'
 })
 export class SubmissionDocsComponent {
-
+  formData = {
+    cinR: null,
+    cinV: null,
+    bacR: null,
+    bacV: null,
+    rnBac: null,
+    diplome: null,
+    rnS1: null,
+    rnS2: null,
+    rnS3: null,
+    rnS4: null,
+    photo: null,
+    demande: null
+  };
+  constructor() {}
+  onSubmit() {
+    console.log(this.formData);
+    // Handle form submission here
+  }
 }
