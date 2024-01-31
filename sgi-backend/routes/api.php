cc21935324f183d6588ce55ce6b3f8818f0ea628
wizardy <wizardhassan99@gmail.com>
2024-01-31 23:01:00 +0100

    commit

--- sgi-backend/routes/api.php
+++ sgi-backend/routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BaccalaureatController;
+use App\Http\Controllers\DiplomeController;
+use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\FeedbackController;
+use App\Http\Controllers\FormationController;
+use App\Http\Controllers\InscriptionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,19 +26,28 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('users', 'UserController');
-Route::resource('feedback', 'FeedbackController');
-Route::resource('documents', 'DocumentController');
-Route::resource('formations', 'FormationController');
-Route::resource('inscriptions', 'InscriptionController');
-Route::resource('baccalaureats', 'BaccalaureatController');
-Route::resource('diplomes', 'DiplomeController');
+// Route::resource('users', 'UserController');
+// Route::resource('feedback', 'FeedbackController');
+// Route::resource('documents', 'DocumentController');
+// Route::resource('formations', 'FormationController');
+// Route::resource('inscriptions', 'InscriptionController');
+// Route::resource('baccalaureats', 'BaccalaureatController');
+// Route::resource('diplomes', 'DiplomeController');
 
-Route::group(['middleware' => 'api'], function ($router) {
+// Route::group(['middleware' => 'api'], function ($router) {
 
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('me', [AuthController::class, 'me']);
+//     Route::post('login', [AuthController::class, 'login']);
+//     Route::post('logout', [AuthController::class, 'logout']);
+//     Route::post('refresh', [AuthController::class, 'refresh']);
+//     Route::post('me', [AuthController::class, 'me']);
 
-});
+// });
+
+
+Route::apiResource('users', UserController::class);
+Route::apiResource('documents', DocumentController::class);
+Route::apiResource('feedbacks', FeedbackController::class);
+Route::apiResource('formations', FormationController::class);
+Route::apiResource('inscriptions', InscriptionController::class);
+Route::apiResource('diplomes', DiplomeController::class);
+Route::apiResource('baccalaureats', BaccalaureatController::class);
