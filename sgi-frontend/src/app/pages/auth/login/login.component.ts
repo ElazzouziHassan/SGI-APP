3977aecd6b85e16f6835579c71fe77d341cc4744
wizardy <wizardhassan99@gmail.com>
2024-02-01 23:22:14 +0100

    commit

--- sgi-frontend/src/app/pages/auth/login/login.component.ts
+++ sgi-frontend/src/app/pages/auth/login/login.component.ts
@@ -6,20 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
   public form = {
+    
     email: null,
     password: null
+
   };
+
   get isEmailEmpty() {
     return !this.form.email;
   }
+
   get isPasswordEmpty() {
     return !this.form.password;
   }
   
   constructor() {}
+
   login() {
     console.log(this.form);
     // TODO: Implement login functionality.
   }
+  
 }
